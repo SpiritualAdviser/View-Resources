7ddfd150df4f1415e95caa97797167d7a30dcec6
SpiritualAdviser <pomaxam@narod.ru>
2025-01-12 09:32:06 +0300

    fix position MaterialAlertDialog

--- app/src/main/java/otus/gpb/homework/viewandresources/MainActivity.kt
+++ app/src/main/java/otus/gpb/homework/viewandresources/MainActivity.kt
@@ -2,11 +2,9 @@ package otus.gpb.homework.viewandresources
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.textfield.TextInputEditText
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,29 +24,5 @@ class MainActivity : AppCompatActivity() {
                 .setView(R.layout.dialog_signin)
                 .show()
         }
-
-//        var textInputEditPassword= findViewById<TextInputEditText>(R.id.editTextPassword)
-
-//        textInputEditTextPassword.transformationMethod = object : PasswordTransformationMethod() {
-//            override fun getTransformation(
-//                source: CharSequence?,
-//                view: View?
-//            ): CharSequence {
-//                return PasswordCharSequence(super.getTransformation(source, view))
-//            }
-//
-//            inner class PasswordCharSequence(private val source: CharSequence) : CharSequence {
-//                override val length: Int get() = source.length
-//
-//                override fun get(index: Int): Char {
-//                    //Only modify char '\u2022' if you want to keep the default behaviour
-//                    return if(source[index] == '\u2022') '*' else source[index]
-//                }
-//
-//                override fun subSequence(startIndex: Int, endIndex: Int): CharSequence {
-//                    return source.subSequence(startIndex, endIndex)
-//                }
-//            }
-//        }
     }
 }
